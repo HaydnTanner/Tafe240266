ac57a880d8b24ae4d3a2de68c52461c8d84be39b
Haydn_Tanner <haydn.tanner@student.tafesa.edu.au>
2024-08-26 13:38:49 +0930

    Started on the calculation for currency

--- UniversalCalculator/CurrencyPage.xaml.cs
+++ UniversalCalculator/CurrencyPage.xaml.cs
@@ -22,9 +22,79 @@ namespace Calculator
 	/// </summary>
 	public sealed partial class CurrencyPage : Page
 	{
+
+		private CurrencyValues FromUSD = new CurrencyValues(1, 0.85189982, 0.72872436, 74.257327, "$", "US Dollars");
+		private CurrencyValues FromEuro = new CurrencyValues(1.1739732, 1, 0.8556672, 87.00755, "€", "Euros");
+		private CurrencyValues FromBritishPound = new CurrencyValues(1.371907, 1.1686692, 1, 101.68635, "£", "British Pounds");
+		private CurrencyValues FromIndianRupee = new CurrencyValues(0.011492628, 0.013492774, 0.0098339397, 1, "₹", "Indian Rupees");
+
+		enum CurrencyIDs
+		{
+			USDollar = 0,
+			Euro = 1,
+			BritishPound = 2,
+			IdianRupee = 3
+		}
+
+
 		public CurrencyPage()
 		{
 			this.InitializeComponent();
 		}
+
+		private void CalculateButton_Click(object sender, RoutedEventArgs e)
+		{
+
+			//error checking for currency input
+
+			CurrencyValues SelectedValues = null;
+
+			switch (ConvertFromComboBox.SelectedIndex)
+			{
+				case (int)CurrencyIDs.USDollar:
+					SelectedValues = FromUSD;
+					break;
+				case (int)CurrencyIDs.Euro:
+					SelectedValues = FromEuro;
+					break;
+				case (int)CurrencyIDs.BritishPound:
+					SelectedValues = FromBritishPound;
+					break;
+				case (int)CurrencyIDs.IdianRupee:
+					SelectedValues = FromIndianRupee;
+					break;
+			}
+
+			if (SelectedValues == null)
+				return;
+
+			double Converted = ConvertCurrency(SelectedValues, ConvertToComboBox.SelectedIndex, double.Parse(CurrencyInput.Text));
+
+
+			UserCurrencyOutput.Text = $"{CurrencyInput.Text} {SelectedValues.Name} =";
+			UserCurrencyConversionOutput.Text = $"{Converted} ";
+
+
+		}
+
+
+		private double ConvertCurrency(CurrencyValues From, int To, double toConvert)
+		{
+			switch(To)
+			{
+				case (int)CurrencyIDs.USDollar:
+					return toConvert * From.USD;
+				case (int)CurrencyIDs.Euro:
+					return toConvert * From.Euro;
+				case (int)CurrencyIDs.BritishPound:
+					return toConvert * From.BritishPound;
+				case (int)CurrencyIDs.IdianRupee:
+					return toConvert * From.IndianRupee;
+			}
+
+			return -1;
+		}
+
+
 	}
 }
